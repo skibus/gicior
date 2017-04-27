7a38e85ee76563c12d67d51d031b3f80ccb90cb4
Ziemo Andrzejewski <andrzejewski.ziemowit@gmail.com>
2017-04-27 09:31:51 +0200

    introduce change

--- gittie.py
+++ gittie.py
@@ -1,11 +1,10 @@
 def introduce():
-    print ('Hello')
+    print ('Witam')
 
 <<<<<<< HEAD
 def add(a,b):
     return a + b
-    
+
 =======
-de function joke():
-    print ('joke')
->>>>>>> 542f496c99c748f30f3ae561e4ff82c7508f324b
+def function joke():
+    return ('joke')
